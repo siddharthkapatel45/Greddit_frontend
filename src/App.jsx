84fa97c74dc3f11f47357a613870aca7ca16faee
Siddharth <siddharthkachhia45@gmil.com>
2025-01-22 22:00:38 +0530

    update all

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,51 @@
-// import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Cookies from "js-cookie";
 import Navbar_OG from "./components/Navbar_OG";
 import Profile from './components/Profile';
 import Create from './components/Create';
 import Home from "./components/Home";
-import Login  from './components/Login';
+import Login from './components/Login";
 import Signup from './components/Signup';
 import EditProfile from './components/Edit_Profile';
 import CreateCommunity from './components/CreateCommunity';
 import CommunityMembers from './components/CommunityMembers';
 import Accept from './components/Accept';
+
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Check for the authentication token in cookies
+    const token = Cookies.get("authToken");
+    setIsLoggedIn(!!token); // Set to true if token exists, otherwise false
+  }, []);
+
   return (
     <Router>
-      <Navbar_OG />
-      <div className="mt-14 p-4">
+      {/* Show Navbar only if logged in */}
+      {isLoggedIn && <Navbar_OG />}
+      <div className={isLoggedIn ? "mt-14 p-4" : ""}>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile/*" element={<Profile />}>
-            {/* <Route path="post" element={<Post />} />
-            <Route path="comment" element={<Comment />} /> */}
-          </Route>
-          <Route path="/create" element={<Create />} />
+          {/* Public Routes */}
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/profile/edit_profile" element={<EditProfile />} />
-          <Route path="/create-community" element={<CreateCommunity />} />
-          <Route path='/community-names' element={<CommunityMembers />} />
-          <Route path='/accept' element={<Accept />} />
+
+          {/* Protected Routes */}
+          {isLoggedIn ? (
+            <>
+              <Route path="/" element={<Home />} />
+              <Route path="/profile/*" element={<Profile />} />
+              <Route path="/create" element={<Create />} />
+              <Route path="/profile/edit_profile" element={<EditProfile />} />
+              <Route path="/create-community" element={<CreateCommunity />} />
+              <Route path='/community-names' element={<CommunityMembers />} />
+              <Route path='/accept' element={<Accept />} />
+            </>
+          ) : (
+            // Redirect to login if not logged in
+            <Route path="*" element={<Navigate to="/login" replace />} />
+          )}
         </Routes>
       </div>
     </Router>
